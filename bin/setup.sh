bba87268820029039e1aacc9235f8e5fe373e975
munuhee <murichustephen@gmail.com>
2023-11-23 18:28:52 +0300

    Impliment Docker setup script

---
+++ bin/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+git clone https://github.com/munuhee/shopping-cart-service.git
+
+cd shopping-cart-service
+
+echo "🔨 Building the Docker image..."
+
+docker build -t shopping-cart-service:latest .
+
+docker run -d -p 8080:8080 -e FLASK_ENV=testing shopping-cart-service
+
+echo "🚀 The shopping-cart-service Flask app is now running."
+echo "🌐 You can access it by opening a web browser and entering:"
+echo "   🌍 http://localhost:8080"
+echo "   or"
+echo "   🌐 http://YOUR_SERVER_IP:8080 (if accessing remotely)"
