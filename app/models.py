a3a25e2cf8012a8b62c7d5a75bdf210b6603cfb4
munuhee <murichustephen@gmail.com>
2023-11-23 15:49:22 +0300

    Improve readability

--- app/models.py
+++ app/models.py
@@ -1,5 +1,9 @@
+"""Defines the CartItem model for a shopping cart."""
+
 from app import db
+
 class CartItem(db.Model):
+    """Model representing items in a shopping cart."""
     __tablename__ = 'cart_items'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -8,4 +12,7 @@ class CartItem(db.Model):
     price = db.Column(db.Float, nullable=False)
 
     def __repr__(self):
+        """
+        Returns a string representation of the CartItem object.
+        """
         return f"<CartItem {self.id}: {self.product_name}>"
